a4140887dfa431bdb70115abfccffe9f76f36c30
Quan Dang <18520339@gm.uit.edu.vn>
2021-10-23 17:12:26 +0700

    beautify

--- Evaluate sorting algorithms/Source.cpp
+++ Evaluate sorting algorithms/Source.cpp
@@ -12,110 +12,108 @@
 using namespace std;
 
 void generate_numbers(int n, int start_num, int end_num, int choice, const char* file_name, const char* file_mode) {
-	FILE* f = fopen(file_name, file_mode);
+    FILE* f = fopen(file_name, file_mode);
 
-	if (choice == 1) {
-		for (int i = 1; i <= n; ++i) fprintf(f, "%d\n", i);
-		cout << "\nNum range with N = " << n << " and Ascending type has been written to " << file_name;
-	}
-	else if (choice == 2) {
-		for (int i = n; i >= 1; --i) fprintf(f, "%d\n", i);
-		cout << "\nNum range with N = " << n << " and Descending type has been written to " << file_name;
-	}
-	else if (choice == 3) {
-		srand(time(0));
-		for (int i = 1; i <= n; ++i) {
-			int x = start_num + rand() % (end_num - start_num + 1);
-			fprintf(f, "%d\n", x);
-		}
-		cout << "\nNum range with N = " << n << " and Randomize type has been written to " << file_name;
-	}
-
-	fclose(f);
+    if (choice == 1) {
+        for (int i = 1; i <= n; ++i) fprintf(f, "%d\n", i);
+        cout << "\nNum range with N = " << n << " and Ascending type has been written to " << file_name;
+    } else if (choice == 2) {
+        for (int i = n; i >= 1; --i) fprintf(f, "%d\n", i);
+        cout << "\nNum range with N = " << n << " and Descending type has been written to " << file_name;
+    } else if (choice == 3) {
+        srand(time(0));
+        for (int i = 1; i <= n; ++i) {
+            int x = start_num + rand() % (end_num - start_num + 1);
+            fprintf(f, "%d\n", x);
+        }
+        cout << "\nNum range with N = " << n << " and Randomize type has been written to " << file_name;
+    }
+    fclose(f);
 }
 
 bool read_from_file(const char* file_name, const char* file_mode, int*& arr, int n) {
-	FILE* f = fopen(file_name, file_mode);
+    FILE* f = fopen(file_name, file_mode);
 
-	if (f != NULL) {
-		arr = new int[n];
-		for (int i = 0; i < n; ++i) fscanf(f, "%d", &arr[i]);
-		fclose(f);
-		return true;
-	}
+    if (f != NULL) {
+        arr = new int[n];
+        for (int i = 0; i < n; ++i) fscanf(f, "%d", &arr[i]);
+        fclose(f);
+        return true;
+    }
 
-	cout << "Cannot found " << file_name;
-	return false;
+    cout << "Cannot found " << file_name;
+    return false;
 }
 
 void export_sorted_file(int* arr, int n, const char* file_name, const char* file_mode) {
-	FILE* f = fopen(file_name, file_mode);
-	for (int i = 0; i < n; ++i)
-		fprintf(f, "%d\n", arr[i]);
-	fclose(f);
+    FILE* f = fopen(file_name, file_mode);
+    for (int i = 0; i < n; ++i)
+        fprintf(f, "%d\n", arr[i]);
+    fclose(f);
 }
 
 int main() {
-	FILE* f = fopen("Results\\Radix Sort.txt", "w");
-	fprintf(f, "\t\t\tRadix Sort");
-	fprintf(f, "\nN\tAscending\tDescending\tRandomize\tAverage");
+    FILE* f = fopen("Results\\Radix Sort.txt", "w");
+    fprintf(f, "\t\t\tRadix Sort");
+    fprintf(f, "\nN\tAscending\tDescending\tRandomize\tAverage");
 
-	for (int n = 1000; n <= 21000; n += 2000) {
-		fprintf(f, "\n%d\t", n);
+    for (int n = 1000; n <= 21000; n += 2000) {
+        fprintf(f, "\n%d\t", n);
 
-		char buffer[6];
-		_itoa(n, buffer, 10);
+        char buffer[6];
+        _itoa(n, buffer, 10);
 
-		char* s1 = new char[6];
-		s1 = (char*)buffer;
+        char* s1 = new char[6];
+        s1 = (char*)buffer;
 
-		char* s2 = new char[6];
-		s2 = (char*)".txt";
+        char* s2 = new char[6];
+        s2 = (char*)".txt";
 
-		int len1 = strlen(s1);
-		int len2 = strlen(s2);
+        int len1 = strlen(s1);
+        int len2 = strlen(s2);
+        int total_length = len1 + len2 + 1;
 
-		// create s3 with sufficient length and character '\ 0'
-		char* file_name = new char[len1 + len2 + 1];
-		strcpy(file_name, s1);
+        // create s3 with sufficient length and character '\ 0'
+        char* file_name = new char[total_length];
+        strcpy(file_name, s1);
 
-		// move the s3 pointer backward to make strcat run faster
-		char* s3 = file_name + len1;
-		strcat(s3, s2);
+        // move the s3 pointer backward to make strcat run faster
+        char* s3 = file_name + len1;
+        strcat(s3, s2);
 
-		double total_time = 0;
-		for (int i = 1; i <= 3; ++i) {
-			// Generate data and write to file
-			// generate_numbers(n, -1000, 1000, i, file_name, "w"); 
-			// => Found that Radix Sort cannot handle negative
-			generate_numbers(n, 0, 1000, i, file_name, "w");
+        double total_time = 0;
+        for (int i = 1; i <= 3; ++i) {
+            // Generate data and write to file
+            // generate_numbers(n, -1000, 1000, i, file_name, "w"); 
+            // => Found that Radix Sort cannot handle negative
+            generate_numbers(n, 0, 1000, i, file_name, "w");
 
-			int* arr;
-			bool file_existed = read_from_file(file_name, "r", arr, n);
+            int* arr;
+            bool file_existed = read_from_file(file_name, "r", arr, n);
 
-			if (file_existed) {
-				clock_t start = clock();
-				radix_sort(arr, n);
-				clock_t end = clock();
+            if (file_existed) {
+                clock_t start = clock();
+                radix_sort(arr, n);
+                clock_t end = clock();
 
-				double execute_time = double(end - start);  // / CLOCKS_PER_SEC;
-				total_time += execute_time;
+                double execute_time = double(end) - double(start);  // / CLOCKS_PER_SEC;
+                total_time += execute_time;
 
-				fprintf(f, "%.4lf ms\t", execute_time);
-				cout << "\nWith N = " << n << " => Radix Sort runs " << i << " times - Costs: " << (size_t)execute_time << " ms";
+                fprintf(f, "%.4lf ms\t", execute_time);
+                cout << "\nWith N = " << n << " => Radix Sort runs " << i << " times - Costs: " << (size_t)execute_time << " ms";
 
-				export_sorted_file(arr, n, file_name, "w");
-				delete[] arr;
-			}
-		}
+                export_sorted_file(arr, n, file_name, "w");
+                delete[] arr;
+            }
+        }
 
-		fprintf(f, "%.4lf ms", total_time / 3);
-		cout << endl;
-	}
+        fprintf(f, "%.4lf ms", total_time / 3);
+        cout << endl;
+    }
 
-	cout << "\nAlgorithm runtime result has been written to file\n";
-	fclose(f);
+    cout << "\nAlgorithm runtime result has been written to file\n";
+    fclose(f);
 
-	system("pause");
-	return 0;
+    system("pause");
+    return 0;
 }
--- Evaluate sorting algorithms/utils.cpp
+++ Evaluate sorting algorithms/utils.cpp
@@ -1,59 +1,62 @@
 ﻿#pragma once
 
 void swap(int& a, int& b) {
-	int temp = a;
-	a = b;
-	b = temp;
+    int temp = a;
+    a = b;
+    b = temp;
 }
 
 void interchange_sort(int* arr, int n) {
-	for (int i = 0; i < n - 1; ++i) {
-		for (int j = i + 1; j < n; ++j) {
-			if (arr[i] > arr[j])
-				swap(arr[i], arr[j]);
-		}
-	}
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = i + 1; j < n; ++j) {
+            if (arr[i] > arr[j])
+                swap(arr[i], arr[j]);
+        }
+    }
 }
 
 void quick_sort(int* arr, int left, int right) {
-	int i = left;
-	int j = right;
-	int middle = arr[(left + right) / 2];
+    int i = left;
+    int j = right;
+    int middle = arr[(left + right) / 2];
 
-	do {
-		while (arr[i] < middle && i < right) i++;
-		while (arr[j] > middle && j > left) j--;
+    do {
+        while (arr[i] < middle && i < right) i++;
+        while (arr[j] > middle && j > left) j--;
 
-		if (i <= j) {
-			swap(arr[i], arr[j]);
-			i++;
-			j--;
-		}
-	} while (i <= j);
+        if (i <= j) {
+            swap(arr[i], arr[j]);
+            i++;
+            j--;
+        }
+    } while (i <= j);
 
-	if (left < j) quick_sort(arr, left, j);
-	if (i < right) quick_sort(arr, i, right);
+    if (left < j) quick_sort(arr, left, j);
+    if (i < right) quick_sort(arr, i, right);
 }
 
 void radix_sort(int arr[], int n) {
-	int max = arr[0];
-	for (int i = 1; i < n; ++i)
-		if (arr[i] > max) max = arr[i];
+    int max = arr[0];
+    for (int i = 1; i < n; ++i)
+        if (arr[i] > max) max = arr[i];
 
-	for (int exp = 1; max / exp > 0; exp *= 10) {
-		int* arr_temp = new int[n];
-		int i, count[10] = { 0 };
+    for (int exp = 1; max / exp > 0; exp *= 10) {
+        int* arr_temp = new int[n];
+        int i, count[10] = { 0 };
 
-		for (i = 0; i < n; i++) count[(arr[i] / exp) % 10]++;
-		for (i = 1; i < 10; i++) count[i] += count[i - 1];
+        for (i = 0; i < n; i++) 
+            count[(arr[i] / exp) % 10]++;
 
-		for (i = n - 1; i >= 0; i--) {
-			arr_temp[count[(arr[i] / exp) % 10] - 1] = arr[i];
-			count[(arr[i] / exp) % 10]--;
-		}
+        for (i = 1; i < 10; i++) 
+            count[i] += count[i - 1];
 
-		for (i = 0; i < n; i++) arr[i] = arr_temp[i];
-		delete[] arr_temp;
-	}
-		
+        for (i = n - 1; i >= 0; i--) {
+            int temp = (arr[i] / exp) % 10;
+            arr_temp[count[temp] - 1] = arr[i];
+            count[temp]--;
+        }
+
+        for (i = 0; i < n; i++) arr[i] = arr_temp[i];
+        delete[] arr_temp;
+    }
 }
